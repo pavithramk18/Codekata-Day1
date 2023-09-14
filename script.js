7fa949a4897c38b01a462ef224073303ff64f3af
pavithramk18 <pavithram.kumaresan@gmail.com>
2023-09-14 19:26:59 +0530

    codekata day1

---
+++ script.js
@@ -0,0 +1,64 @@
+//Variables
+
+//Definition:Variables are containers used for storing the value
+
+// Syntax:variabletype variablename = variable value;
+//Example var string="pavi";
+
+// String:Anything which is enclosed on double quotes is always called as string even if their is numbers enclosed in "123" are also called as strings
+
+// console.log("Hello");  "Hello"
+
+// var fruits ="apple";
+// console.log(fruits);    "apple"
+
+// var batch = "B52";
+// console.log(batch);   "B52"
+ 
+
+// var number =12;
+// console.log(number);  12
+
+//  var name1 = "Pavithra";
+//  var batch = "B52 WD Tamil";
+// var placement = "Placed at CTS"
+
+// console.log(name1);  "Pavithra"
+// console.log(batch); "B52 WD Tamil"
+//  console.log(placement);  "Placed at CTS"
+//  console.log(name1 + batch +placement)    PavithraB52 WD TamilPlaced at CTS
+
+
+// Types of variables
+
+//  1.var
+// 2.let
+//  3.const
+
+//  String Methods
+
+//  Convert Number to String  -->  toString()
+
+// var numbers=1234;
+// console.log(typeof(numbers)); Type-->  number
+// var tostr= numbers.toString();  //Coverting number to string this method is used
+// console.log(tostr);   1234
+// console.log(typeof(tostr)); //For checking whether the number have been changed ---->String
+
+// To find the length of the String-->Returns the number of character in the enclosed string
+
+//Length of a string starts from 1 and it will also includes space also a character while taking the length
+
+
+// var str= "Pavithra ";
+// console.log(str.length);   9
+
+// var friend  = "I have very few number of friends on my circle";
+// console.log(friend.length);  46
+
+
+
+// //To change to upper case
+
+// //To change to lower case
+
